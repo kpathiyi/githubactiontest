d6ebc6fbc2360efbee333cd84a66ef27a9c365ef
kpath <kpath201@gmail.com>
2025-10-05 13:53:34 -0400

    springboot 3.5.6

---
+++ src/main/java/com/example/githubactiontest/GithubactiontestApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactiontestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactiontestApplication.class, args);
+	}
+
+}
