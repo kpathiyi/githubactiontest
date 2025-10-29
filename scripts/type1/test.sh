6b7d5840bb97737c2633f10bfca953a5779ec38c
kpath <kpath201@gmail.com>
2025-10-28 20:42:03 -0400

    try to load env vars from dev.sh

--- scripts/type1/test.sh
+++ scripts/type1/test.sh
@@ -7,8 +7,8 @@ echo "DB_USER [$DB_PWD] from test.sh before source command"
 
 source envs/$env_name.sh
 
-echo "DB_USER [$DB_USER] from test.sh"
-echo "DB_USER [$DB_PWD] from test.sh"
+echo "DB_USER [$DB_USER] from test.sh after source command"
+echo "DB_USER [$DB_PWD] from test.sh after source command"
 
 export DB_USER2=$DB_USER
 export DB_PWD2=$DB_PWD
